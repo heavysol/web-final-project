9b0ca386ca8c9dc57f35853806775e1488040b31
unknown <bigjolemaire@gmail.com>
2024-12-08 11:35:34 +0000

    login : mis style de page

--- views/login.php
+++ views/login.php
@@ -4,14 +4,14 @@
 	<title> Log in - Moreish </title>
 	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
 	<link rel = 'stylesheet' href = '../assets/css/general-styles.css'>
-	<link rel = 'stylesheet' href = '../assets/css/logsign-styles.css'>
+	<!--<link rel = 'stylesheet' href = '../assets/css/logsign-styles.css'>-->
 	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
 </head>
 
-<body>
-	<!-- Navbar -->
-	<div class = 'screen'>
-		salut
+<body class = 'bg-dark p-3'>
+	<!-- Screen -->
+	<div class = 'set-screen-width-height container p-5 my-5 border border-3 rounded bg-secondary'>
+		salut fuck
 	</div>
 		
 
