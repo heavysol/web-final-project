6648a9ce5a9272d30711035e6ee23c59051dbdd6
unknown <bigjolemaire@gmail.com>
2024-12-08 13:00:41 +0000

    login : fini formulaire basiquement

--- views/login.php
+++ views/login.php
@@ -8,10 +8,36 @@
 	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
 </head>
 
-<body class = 'bg-dark p-3'>
+<body class = 'bg-secondary p-3 text-white'>
 	<!-- Screen -->
-	<div class = 'set-screen-width-height container p-5 my-5 border border-3 rounded bg-secondary'>
-		salut fuck
+	<div class = 'set-screen-width-height container p-5 my-5 border border-3 rounded bg-dark'>
+		<section>
+			<h1> Log in </h1>
+			<form action = ''>
+				<div>
+					<label for = 'uname' class = 'form-label'> Username </label>
+					<input type = 'text' class = 'form-control' name = 'uname' placeholder = 'Enter username, like apple123' required>
+				</div>
+
+				<div>
+					<label for = 'passw' class = 'form-label'> Password </label>
+					<input type = 'password' class = 'form-control' name = 'passw' placeholder = 'Enter password' minlength = 8 required>
+				</div>
+
+				<div>
+					<label for = 'passw-con' class = 'form-label'> Confirm password </label>
+					<input type = 'password' class = 'form-control' name = 'passw-con' placeholder = 'Confirm password' minlength = 8 required>
+				</div>
+
+				<div>
+					<label for = 'email' class = 'form-label'> Email </label>
+					<input type = 'email' class = 'form-control' name = 'email' placeholder = 'Enter email address, like jj@gmail.com' required>
+				</div>
+
+				<br> 
+
+				<button class = 'btn btn-light position-absolute start-50 translate-middle-x' name = 'login'> Log in </button>
+			</form>
 	</div>
 		
 
