39abefda7951a8f8d337165ef44d612674d8c43e
unknown <bigjolemaire@gmail.com>
2024-12-08 10:51:18 +0000

    css : ajouté bootstrap

--- views/login.php
+++ views/login.php
@@ -5,6 +5,7 @@
 	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
 	<link rel = 'stylesheet' href = '../assets/css/general-styles.css'>
 	<link rel = 'stylesheet' href = '../assets/css/logsign-styles.css'>
+	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
 </head>
 
 <body>
