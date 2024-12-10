12dd727813a3898a8a49b688210ef386526007d0
unknown <bigjolemaire@gmail.com>
2024-12-10 14:48:32 +0000

    index : rendu page vide

--- index.php
+++ index.php
@@ -1,41 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- NB: recipe-feed.html is in pages folder -->
-	<title> Home - Moreish </title>
+<title> Log in </title>
 	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
-	<link rel = 'stylesheet' href = './styles/general-styles.css'>
-    <link rel = 'stylesheet' href = './styles/index-styles.css'>
+	<link rel = 'stylesheet' href = '../assets/css/general-styles.css'>
+	<!--<link rel = 'stylesheet' href = '../assets/css/logsign-styles.css'>-->
+	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
 </head>
  
 <body>
-	<!-- Navbar -->
-	<nav class = 'navbar'>
-		<a href = './index.html'> <img src = './assets/pics/logo.png' alt = 'Moreish logo' class = 'app-logo-nav'> </a>
-		<div class = 'navbar-right'>
-			<a class = 'nav-op' href = './user-pages/login.html'> Log in </a>
-			<a class = 'nav-op' href = './user-pages/signup.html'> Sign up </a>
-		</div>
-	</nav>
+<body class = 'bg-secondary p-3 text-white'>
+	<!-- Icon -->
+	<img src = '../assets/images/logo.png' alt = 'site logo' class = 'mx-auto d-block'>
+	
+	<!-- Screen -->
+	<div class = 'container p-5 my-5 border border-3 rounded bg-dark'>
+		<section>
+			<h1>  </h1>
+			
+	</div>
+		
 
-    <!-- Heading section -->
-    <section>
-       <img src = './assets/pics/logo.png' alt = 'Moreish logo' class = 'app-logo-home'> <h2 class = 'index-upper-text'>The law here: 'just one more recipe!'</h2>
-    </section>
-        
-    <!-- Body section -->
-    <section>
-        <p class = 'index-lower-text'>Moreish is an online platform for you to share, view and comment on recipes from our culinary users: professional chefs, zealous amateurs - and you!</p>
-        <a class = 'index-get-started' href = './user-pages/recipe-feed.html'> Get started! </a>
+	<footer>
+		
+	</footer>
 
-        <p class = 'index-lower-text'>Already a Moreish registered user? <a class = 'index-button' href = './user-pages/login.html'>Log in</a></p>
-        <p class = 'index-lower-text'>Newcomer to the Moreish kitchen? <a class = 'index-button' href = './user-pages/signup.html'>Sign up</a></p>
-    </section>
-
-    <footer>
-        Copyright - Lemaire Prod
-    </footer>
-
-    <!-- Created on 23 Sep '24 -->
+	<!--<script src = '../scripts/logsign-scripts/login-script.js'></script>-->
+	<!-- Created on 14 Sep '24 -->
 </body>
 </html>
