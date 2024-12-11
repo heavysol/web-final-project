b07ef75deb5ee98d78bc79f5af4bec28ff79f2fc
unknown <bigjolemaire@gmail.com>
2024-12-11 15:21:10 +0000

    images : ajouté thumbnails de jeux

--- views/regular-dashboard.php
+++ views/regular-dashboard.php
@@ -7,7 +7,7 @@
 	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
 </head>
  
-<body class = 'bg-secondary p-3 text-white' onload = 'showContent()'>
+<body class = 'bg-secondary p-3 text-white'>
 	<!-- Icon -->
 	<img src = '../assets/images/logo.png' alt = 'site logo' class = 'mx-auto d-block'>
 
@@ -16,9 +16,25 @@
 	
 	<!-- Screen -->
 	<div class = 'container p-5 my-5 border border-3 rounded bg-dark'>
-		<div id = 'content'></div>
+        <!-- Navbar -->
+		<nav class = 'navbar navbar-expand-sm bg-dark navbar-dark border border-1 border-white rounded'>
+            <div class = 'container-fluid'>
+                <ul class = 'navbar-nav'>
+                    <li class = 'nav-item'>
+                        <a href = './regular-dashboard.php' class = 'nav-link'> Home </a>
+                    </li>
+                    <li class = 'nav-item'>
+                        <a href = '#' class = 'nav-link'> Games </a>
+                    </li>
+                    <li class = 'nav-item'>
+                        <a href = '#' class = 'nav-link'> Leaderboard </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        
 	</div>
 		
-	<script src = '../assets/js/index-content.js'></script>
 </body>
 </html>
