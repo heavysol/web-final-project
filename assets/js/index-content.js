efbe14eb5d7c219ac74c778bcae615d7cb9ec48c
unknown <bigjolemaire@gmail.com>
2024-12-11 13:59:35 +0000

    index-content : crée home_content et btns

--- assets/js/index-content.js
+++ assets/js/index-content.js
@@ -1,7 +1,10 @@
 /* Manages print of text and img content on index.html (homepage) */
 
 const logo = `<img src = '../assets/images/logo_white.png' alt = 'site logo in white' class = 'mx-auto d-block'>`;
-let loading_spinner = `<div class = 'mx-auto d-block spinner-border'></div>`;
+const loading_spinner = `<div class = 'mx-auto d-block spinner-border'></div>`;
+const home_content = `<div class = 'mx-auto d-block text-center'> <h3>Welcome to the GameStation console experience - games and fun galore. What would you like to do?</h3> </div> <br>`;
+const btns = `<a href = './login.php' class = 'btn btn-light'>Log in</a> <a href = './signup.php' class = 'btn btn-light'>Sign up</a>`;
+
 function showContent() {
     document.getElementById('content').innerHTML = `${logo}<br>${loading_spinner}`;
 }
