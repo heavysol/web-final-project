a211ed47e011239b4710f4cf59c6b226f3d962fe
unknown <bigjolemaire@gmail.com>
2024-12-11 14:09:42 +0000

    index-content : ajouté avec succès l'écran de chargement

--- assets/js/index-content.js
+++ assets/js/index-content.js
@@ -2,10 +2,13 @@
 
 const logo = `<img src = '../assets/images/logo_white.png' alt = 'site logo in white' class = 'mx-auto d-block'>`;
 const loading_spinner = `<div class = 'mx-auto d-block spinner-border'></div>`;
-const home_content = `<div class = 'mx-auto d-block text-center'> <h3>Welcome to the GameStation console experience - games and fun galore. What would you like to do?</h3> </div> <br>`;
-const btns = `<a href = './login.php' class = 'btn btn-light'>Log in</a> <a href = './signup.php' class = 'btn btn-light'>Sign up</a>`;
+const home_content = `<div class = 'mx-auto d-block text-center'> <h3>Welcome to the GameStation console experience - games and fun galore. What would you like to do?</h3> </div>`;
+const btns = `<div class = 'position-absolute start-50 translate-middle-x'> <a href = './login.php' class = 'btn btn-light'>Log in</a> <a href = './signup.php' class = 'btn btn-light'>Sign up</a> </div>`;
 
 function showContent() {
     document.getElementById('content').innerHTML = `${logo}<br>${loading_spinner}`;
+    setTimeout(() => {
+        document.getElementById('content').innerHTML = `${home_content}<br>${btns}`;
+    }, 2000);
 }
 showContent();
