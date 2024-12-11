70eb6e928f7be81a39d7b8a13a3230874ad8780c
unknown <bigjolemaire@gmail.com>
2024-12-11 13:11:16 +0000

    index : deplaceant à views

---
+++ views/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Home </title>
+	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
+	<link rel = 'stylesheet' href = '../assets/css/general-styles.css'>
+	<!--<link rel = 'stylesheet' href = '../assets/css/logsign-styles.css'>-->
+	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
+</head>
+ 
+<body class = 'bg-secondary p-3 text-white' onload = 'showContent()'>
+	<!-- Icon -->
+	<img src = '../assets/images/logo.png' alt = 'site logo' class = 'mx-auto d-block'>
+
+    <!-- Console start light -->
+    <div class = 'container'></div>
+	
+	<!-- Screen -->
+	<div class = 'container p-5 my-5 border border-3 rounded bg-dark'>
+		<div id = 'content'></div>
+	</div>
+		
+
+	<footer>
+		
+	</footer>
+
+	<!--<script src = '../assets/js/index-content.js'></script>-->
+</body>
+</html>
