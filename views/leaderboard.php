b805c49e4aea346dc3684b406992299af1545ed2
unknown <bigjolemaire@gmail.com>
2024-12-12 17:21:50 +0000

    leadership : mis code génèrale SANS leaderboard

---
+++ views/leaderboard.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+<head>
+<title> Startup </title>
+	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
+	<link rel = 'stylesheet' href = '../assets/css/general-styles.css'>
+	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
+</head>
+ 
+<body class = 'bg-secondary p-3 text-white'>
+	<!-- Icon -->
+	<img src = '../assets/images/logo.png' alt = 'site logo' class = 'mx-auto d-block'>
+
+    <!-- Console start light -->
+    <div class = 'container'></div>
+	
+	<!-- Screen -->
+	<div class = 'container p-5 my-5 border border-3 rounded bg-dark'>
+        <!-- Navbar -->
+		<nav class = 'navbar navbar-expand-sm bg-dark navbar-dark border border-2 border-secondary rounded'>
+            <div class = 'container-fluid'>
+                <ul class = 'navbar-nav'>
+                    <li class = 'nav-item'>
+                        <a href = './regular-dashboard.php' class = 'nav-link'> Home </a>
+                    </li>
+                    <li class = 'nav-item'>
+                        <a href = './games.php' class = 'nav-link'> Games </a>
+                    </li>
+                    <li class = 'nav-item'>
+                        <a href = './leaderboard.php' class = 'nav-link'> Leaderboard </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <br>
+
+        <!-- Leaderboard --> 
+        <!-- SQL DYMANIQUE CHOSES ICI -->
+	</div>
+</body>
+</html>
