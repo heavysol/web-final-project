97b6b6cbe091c2b22bae199c23984d867a5b643e
unknown <bigjolemaire@gmail.com>
2024-12-12 17:53:25 +0000

    dont-crash-game : mis général code

---
+++ views/dont-crash-game.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+<head>
+<title> Startup </title>
+	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
+	<link rel = 'stylesheet' href = '../assets/css/general-styles.css'>
+	<link rel = 'stylesheet' href = '../assets/css/bootstrap-css/bootstrap.min.css'>
+</head>
+ 
+<body class = 'bg-secondary p-3 text-white'>
+	<!-- Icon -->
+	<img src = '../assets/images/logo.png' alt = 'site logo' class = 'mx-auto d-block'>
+
+    <!-- Console start light -->
+    <div class = 'container'></div>
+	
+	<!-- Screen -->
+	<div class = 'container p-5 my-5 border border-3 rounded bg-dark'>
+        <!-- Game -->
+         
+	</div>
+</body>
+</html>
