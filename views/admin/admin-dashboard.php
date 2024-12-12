5b4f33fb813b2b9dfcf65034a9522ae1eec94bd7
unknown <bigjolemaire@gmail.com>
2024-12-12 17:35:50 +0000

    admin-dashboard : formatté page avec navbar modifié

---
+++ views/admin/admin-dashboard.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+<head>
+<title> Startup </title>
+	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
+	<link rel = 'stylesheet' href = '../../assets/css/general-styles.css'>
+	<link rel = 'stylesheet' href = '../../assets/css/bootstrap-css/bootstrap.min.css'>
+</head>
+ 
+<body class = 'bg-secondary p-3 text-white'>
+	<!-- Icon -->
+	<img src = '../../assets/images/logo.png' alt = 'site logo' class = 'mx-auto d-block'>
+
+    <!-- Console start light -->
+    <div class = 'container'></div>
+	
+	<!-- Screen -->
+	<div class = 'container p-5 my-5 border border-3 rounded bg-dark'>
+        <!-- Navbar -->
+		<nav class = 'navbar navbar-expand-sm bg-dark navbar-dark border border-2 border-secondary rounded'>
+            <div class = 'container-fluid'>
+                <ul class = 'navbar-nav'>
+                    <li class = 'nav-item'>
+                        <a href = './regular-dashboard.php' class = 'nav-link'> Home </a>
+                    </li>
+                    <li class = 'nav-item'>
+                        <a href = './games.php' class = 'nav-link'> Players </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <br>
+
+        <!-- Player records for games --> 
+        
+	</div>
+</body>
+</html>
