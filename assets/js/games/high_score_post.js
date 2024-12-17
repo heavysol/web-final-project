4f0b6f60f941865877e7841ffb8fa856710d0405
unknown <bigjolemaire@gmail.com>
2024-12-17 19:09:30 +0000

    high_score_post : fini postHighScore

---
+++ assets/js/games/high_score_post.js
@@ -0,0 +1,9 @@
+function postHighScore(score) {
+    let xml = new XMLHttpRequest();
+    const url = '../actions/dont-crash_acts.php';
+    const highscore = JSON.stringify({'high-score': score});
+
+    xml.open('POST', url);
+    xml.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xml.send('hs=' + highscore);
+}
