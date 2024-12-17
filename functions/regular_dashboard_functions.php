a986852f592a28b754ea6315c407bee5a00c8c24
unknown <bigjolemaire@gmail.com>
2024-12-17 20:03:29 +0000

    regular_dash : fini viewHighScore

--- functions/regular_dashboard_functions.php
+++ functions/regular_dashboard_functions.php
@@ -8,4 +8,15 @@
         $sql = "INSERT INTO `dont-crash` (high_score) VALUES ('$score') WHERE player_id = '$id'";
         makeSQLQuery($sql);
     }
+
+    function viewHighScore($db) {
+        global $conn;
+        $table = ($db == 1) ? `dont-crash` : `where-seed`;
+        session_start();
+        $id = $_SESSION['id'];
+
+        $sql = "SELECT * FROM $table WHERE player_id = $id";
+        $result = $conn->query($sql)->fetch_assoc();
+        return $result['high_score'];
+    }
 ?>
